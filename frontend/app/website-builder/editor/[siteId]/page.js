a8f8541816664a3abdc68cf83147499da3bb4ccf
sasdanube <gestiondanube@gmail.com>
2026-08-08 20:07:17 +0200

    feat(mse-25.3): load public renderer styles in V2 editor

--- frontend/app/website-builder/editor/[siteId]/page.js
+++ frontend/app/website-builder/editor/[siteId]/page.js
@@ -1,5 +1,8 @@
 import VisualPageBuilder from "../../../../components/page-builder-v2/VisualPageBuilder";
 
+import "../../../sites/[siteSlug]/public-site.css";
+import "../../../../components/public-site/brand-runtime.css";
+
 export const dynamic = "force-dynamic";
 
 export default async function MiniSiteEditorPage({
