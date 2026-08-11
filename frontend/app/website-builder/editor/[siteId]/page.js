2cc5d195730e795382c898f82c71492e0c10cc44
sasdanube <gestiondanube@gmail.com>
2026-08-11 20:17:32 +0200

    feat(mse-25.10): open SEO target in designer context

--- frontend/app/website-builder/editor/[siteId]/page.js
+++ frontend/app/website-builder/editor/[siteId]/page.js
@@ -1,4 +1,5 @@
 import VisualPageBuilder from "../../../../components/page-builder-v2/VisualPageBuilder";
+import SeoDesignerContext from "../../../../components/page-builder-v2/SeoDesignerContext";
 
 import "../../../sites/[siteSlug]/public-site.css";
 import "../../../../components/public-site/brand-runtime.css";
@@ -7,12 +8,21 @@ export const dynamic = "force-dynamic";
 
 export default async function MiniSiteEditorPage({
   params,
+  searchParams,
 }) {
   const resolvedParams = await params;
+  const query = (await searchParams) || {};
 
   return (
-    <VisualPageBuilder
-      siteId={resolvedParams.siteId}
-    />
+    <>
+      <VisualPageBuilder
+        siteId={resolvedParams.siteId}
+      />
+      <SeoDesignerContext
+        pageSlug={query.seoPageSlug || ""}
+        keyword={query.seoKeyword || ""}
+        mode={query.seoMode || "monitor"}
+      />
+    </>
   );
 }
