76c599862c9335a9a7ee7280a84e72b1e86e1933
sasdanube <gestiondanube@gmail.com>
2026-08-14 18:22:42 +0200

    MSE-25.20 add internal performance page

---
+++ frontend/app/indexation/performance/page.js
@@ -0,0 +1,11 @@
+import "../indexation.css";
+import SearchPerformanceClient from "./SearchPerformanceClient";
+
+export const metadata = {
+  title: "Performance Search Console | Marketing Engine",
+  robots: { index: false, follow: false },
+};
+
+export default function SearchPerformancePage() {
+  return <SearchPerformanceClient />;
+}
