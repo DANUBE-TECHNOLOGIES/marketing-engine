58a54cd3096c0425c9d732471a05b363883607de
sasdanube <gestiondanube@gmail.com>
2026-08-14 16:15:23 +0200

    feat(mse-25.16): export google search console provider

--- backend/src/modules/search-console-submission/index.js
+++ backend/src/modules/search-console-submission/index.js
@@ -2,12 +2,19 @@
 
 const { routes } = require("./routes");
 const { SearchConsoleSubmissionService, ACTION_TYPE, MODE } = require("./service");
-const { DisabledSearchConsoleProvider, validateSearchConsoleSubmissionTarget } = require("./provider");
+const {
+  SEARCH_CONSOLE_API_ROOT,
+  DisabledSearchConsoleProvider,
+  GoogleSearchConsoleProvider,
+  validateSearchConsoleSubmissionTarget,
+} = require("./provider");
 
 module.exports = {
   routes,
   SearchConsoleSubmissionService,
   DisabledSearchConsoleProvider,
+  GoogleSearchConsoleProvider,
+  SEARCH_CONSOLE_API_ROOT,
   validateSearchConsoleSubmissionTarget,
   ACTION_TYPE,
   MODE,
