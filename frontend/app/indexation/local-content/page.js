5d29d0fb9597c5587263722c8821446de7fe106d
sasdanube <gestiondanube@gmail.com>
2026-08-15 09:37:17 +0200

    MSE-25.25 add local content uniqueness page

---
+++ frontend/app/indexation/local-content/page.js
@@ -0,0 +1,10 @@
+import LocalContentUniquenessClient from "./LocalContentUniquenessClient";
+
+export const metadata = {
+  title: "Unicité des contenus locaux | Marketing Engine",
+  robots: { index: false, follow: false },
+};
+
+export default function LocalContentUniquenessPage() {
+  return <LocalContentUniquenessClient />;
+}
