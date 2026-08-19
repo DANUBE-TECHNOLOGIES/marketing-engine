164c75dedacade74dba313883ddadd20a62d416d
sasdanube <gestiondanube@gmail.com>
2026-08-19 10:24:32 +0200

    fix(partners): render generated partner page sections

--- frontend/components/public-site/renderers/registry.js
+++ frontend/components/public-site/renderers/registry.js
@@ -1,4 +1,5 @@
 import HeroV2Renderer from "./HeroV2Renderer";
+import PageHeaderRenderer from "./PageHeaderRenderer";
 import CtaV2Renderer from "./CtaV2Renderer";
 import AgencyV2Renderer from "./AgencyV2Renderer";
 import FeaturesV2Renderer from "./FeaturesV2Renderer";
@@ -23,6 +24,7 @@ import TeamRenderer from "./TeamRenderer";
 
 export const PUBLIC_RENDERER_REGISTRY = {
   hero: HeroV2Renderer,
+  "page-header": PageHeaderRenderer,
   cta: CtaV2Renderer,
   "contact-cta": CtaV2Renderer,
   agency: AgencyV2Renderer,
@@ -40,6 +42,7 @@ export const PUBLIC_RENDERER_REGISTRY = {
   intro: RichTextV2Renderer,
   "agency-introduction": RichTextV2Renderer,
   "agency-story": RichTextV2Renderer,
+  "partners-introduction": RichTextV2Renderer,
   image_text: ImageTextV2Renderer,
   "image-text": ImageTextV2Renderer,
   gallery: GalleryV2Renderer,
