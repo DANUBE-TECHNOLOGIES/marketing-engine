f5bab30ce2a4bca6bbc37a4538a35fab3d097f83
sasdanube <gestiondanube@gmail.com>
2026-08-23 00:12:27 +0200

    test(presence): include runtime schema audit suite

--- backend/test/presence-convergence.test.js
+++ backend/test/presence-convergence.test.js
@@ -3,3 +3,4 @@
 require("../src/modules/presence/presence-foundation.test");
 require("../src/modules/presence/presence-convergence.test");
 require("../src/modules/presence/directory-response-adapter.test");
+require("../src/modules/presence/directory-schema-audit.test");
