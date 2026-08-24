c93ec8fc948449a55f0d12274c4a5eb27eff4e7e
sasdanube <gestiondanube@gmail.com>
2026-08-24 19:05:49 +0200

    test(presence): include pilot outcome suite

--- backend/test/presence-convergence.test.js
+++ backend/test/presence-convergence.test.js
@@ -25,6 +25,7 @@ require("../src/modules/presence/runtime-listing-state.test");
 require("../src/modules/presence/operational-readiness.test");
 require("../src/modules/presence/deployment-readiness.test");
 require("../src/modules/presence/pilot-readiness.test");
+require("../src/modules/presence/pilot-outcome.test");
 require("../src/modules/presence/presence-storage-audit.test");
 require("../src/modules/presence/operation-audit.test");
 require("../src/modules/presence/operation-snapshots.test");
