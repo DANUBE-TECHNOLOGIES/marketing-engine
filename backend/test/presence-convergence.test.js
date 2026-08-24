dd926d7f9e049d91b91c7f05c94a70e6aaf4a0fa
sasdanube <gestiondanube@gmail.com>
2026-08-24 13:29:56 +0200

    test(presence): include provider SLA and propagation alert suites

--- backend/test/presence-convergence.test.js
+++ backend/test/presence-convergence.test.js
@@ -25,4 +25,6 @@ require("../src/modules/presence/operation-snapshots.test");
 require("../src/modules/presence/propagation-watch.test");
 require("../src/modules/presence/propagation-control.test");
 require("../src/modules/presence/propagation-dashboard.test");
+require("../src/modules/presence/provider-sla.test");
+require("../src/modules/presence/propagation-alerts.test");
 require("../src/modules/presence/manual-remediation.test");
