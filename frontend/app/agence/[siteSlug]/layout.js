3507e5d106a6f7619d2005c97e0779fae0a1bb0a
sasdanube <gestiondanube@gmail.com>
2026-08-24 12:00:01 +0200

    feat(mse-25.44): connect trust bands to public layout

--- frontend/app/agence/[siteSlug]/layout.js
+++ frontend/app/agence/[siteSlug]/layout.js
@@ -4,6 +4,7 @@ import { publicSiteApi } from "../../../lib/public-site-api";
 import { getPublicBrandTheme } from "../../../lib/public-brand-api";
 import PublicSiteHeader from "../../../components/public-site/PublicSiteHeader";
 import PublicPaymentMethodsBand from "../../../components/public-site/PublicPaymentMethodsBand";
+import PublicInstitutionalTrustBand from "../../../components/public-site/PublicInstitutionalTrustBand";
 import PublicSiteFooter from "../../../components/public-site/PublicSiteFooter";
 import PublicBrandLegalRuntime from "../../../components/public-site/PublicBrandLegalRuntime";
 import JsonLd from "../../../components/JsonLd";
@@ -25,6 +26,7 @@ import "../../../components/public-site/public-readability-fixes.css";
 import "../../../components/public-site/seo-crawlability.css";
 import "../../../components/public-site/desktop-composition.css";
 import "../../../components/public-site/payment-methods-band.css";
+import "../../../components/public-site/institutional-trust-band.css";
 
 export default async function PublicAgencySiteLayout({ children, params }) {
   const { siteSlug } = await params;
@@ -75,6 +77,7 @@ export default async function PublicAgencySiteLayout({ children, params }) {
         <main>{children}</main>
 
         <PublicPaymentMethodsBand />
+        <PublicInstitutionalTrustBand runtime={publicBrandLegalRuntime} />
         <PublicSiteFooter site={site} />
       </div>
     </PublicBrandLegalRuntime>
