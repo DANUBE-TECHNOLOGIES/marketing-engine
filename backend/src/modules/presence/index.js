657e6b34e620c49d9fcb43abb7552883fcea91e1
sasdanube <gestiondanube@gmail.com>
2026-08-24 11:31:11 +0200

    feat(presence): mount network Google remediation routes

--- backend/src/modules/presence/index.js
+++ backend/src/modules/presence/index.js
@@ -7,6 +7,7 @@ const { discoveryRoutes } = require("./discovery-routes");
 const { networkDiscoveryRoutes } = require("./network-discovery-routes");
 const { remediationRoutes } = require("./remediation-routes");
 const { remediationExecutionRoutes } = require("./remediation-execution-routes");
+const { networkGoogleRemediationRoutes } = require("./network-google-remediation-routes");
 const providerRegistry = require("./provider-registry");
 
 function routes({ prisma }) {
@@ -17,6 +18,7 @@ function routes({ prisma }) {
   router.use(networkDiscoveryRoutes({ prisma }));
   router.use(remediationRoutes({ prisma }));
   router.use(remediationExecutionRoutes({ prisma }));
+  router.use(networkGoogleRemediationRoutes({ prisma }));
   return router;
 }
 
