668aa0d6fb7fcb3b7eb6ffed050a14caae3100ea
sasdanube <gestiondanube@gmail.com>
2026-08-24 10:55:34 +0200

    feat(presence): compose core and observation routes

--- backend/src/modules/presence/index.js
+++ backend/src/modules/presence/index.js
@@ -1,7 +1,16 @@
 "use strict";
 
-const { routes } = require("./routes");
+const express = require("express");
+const { routes: coreRoutes } = require("./routes");
+const { observationRoutes } = require("./observation-routes");
 const providerRegistry = require("./provider-registry");
+
+function routes({ prisma }) {
+  const router = express.Router();
+  router.use(coreRoutes({ prisma }));
+  router.use(observationRoutes({ prisma }));
+  return router;
+}
 
 module.exports = {
   routes,
