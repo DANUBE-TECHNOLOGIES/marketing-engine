6a50c4b9396544010bf0562fec1aee93578ea01c
sasdanube <gestiondanube@gmail.com>
2026-09-05 14:51:27 +0200

    MSE-25.125P Register calibration audit routes

--- backend/src/modules/ranking-grid/index.js
+++ backend/src/modules/ranking-grid/index.js
@@ -4,21 +4,25 @@ const routes = require("./routes");
 const baselineRoutes = require("./baseline-routes");
 const paidPlanRoutes = require("./paid-plan-routes");
 const noSearchRoutes = require("./no-search-routes");
+const calibrationRoutes = require("./calibration-routes");
 const { RankingGridRepository } = require("./repository");
 const { RankingGridService } = require("./service");
 const { RankingGridProvider, UnconfiguredRankingGridProvider } = require("./provider");
 const { generateGrid } = require("./grid");
 const { summarizePoints } = require("./aggregate");
+const { buildCalibrationReport } = require("./calibration");
 
 module.exports = {
   routes,
   baselineRoutes,
   paidPlanRoutes,
   noSearchRoutes,
+  calibrationRoutes,
   RankingGridRepository,
   RankingGridService,
   RankingGridProvider,
   UnconfiguredRankingGridProvider,
   generateGrid,
   summarizePoints,
+  buildCalibrationReport,
 };
