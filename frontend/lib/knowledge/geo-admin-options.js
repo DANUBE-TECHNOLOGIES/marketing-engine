020bf2c0e28d87a5c9bca3949feaa38b09fa1a1d
sasdanube <gestiondanube@gmail.com>
2026-09-07 17:34:33 +0200

    MSE-GEO admin centralize GEO Knowledge options

---
+++ frontend/lib/knowledge/geo-admin-options.js
@@ -0,0 +1,10 @@
+export const GEO_KNOWLEDGE_TYPES = [
+  ["agency", "Agence"],
+  ["person", "Personne"],
+  ["expertise", "Expertise"],
+];
+
+export const GEO_KNOWLEDGE_RELATION_TYPES = [
+  ["works_at", "Travaille dans"],
+  ["expert_in", "Expert de"],
+];
