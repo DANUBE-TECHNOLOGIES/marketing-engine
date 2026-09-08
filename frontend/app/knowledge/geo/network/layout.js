bce87b9ac386255d598c4a98d2b94988ab088514
sasdanube <gestiondanube@gmail.com>
2026-09-08 08:18:47 +0200

    MSE-GEO V1.25: add Agency Knowledge matrix navigation

--- frontend/app/knowledge/geo/network/layout.js
+++ frontend/app/knowledge/geo/network/layout.js
@@ -15,6 +15,7 @@ export default function NetworkGeoLayout({ children }) {
         <a href="/knowledge/geo/network">Agences</a>
         <a href="/knowledge/geo/network/people">Conseillers</a>
         <a href="/knowledge/geo/network/expertise">Expertises</a>
+        <a href="/knowledge/geo/network/agency-knowledge">Destinations & thèmes</a>
         <a href="/knowledge/geo/network/readiness">Readiness publique</a>
       </nav>
       {children}
